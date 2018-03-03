31d09302a033955a35005c372eaa9ef6585a36ba
tminto <tarbia@gmail.com>
2018-03-03 15:55:31 -0500

    Added graphical menu
    replaced text menu with graphical menu, put all the images in the
    “content.html” page

--- banff.js
+++ banff.js
@@ -3,7 +3,23 @@ $(document).ready(function() {
 	$('.menuBtnHolder').click(function(){
 		$('ul').toggleClass('displayOff');
 		console.log('entered');
-		})
-	
+		/*HIDE THE MENU ICON*/
+		$(this).addClass('displayOff');
+		/*if the user clicks "x"
+			 /*hide the ul and show the menuBtn again
+		/*else if user clicks any number
+			 /*scroll to the number content zone
+	*/
+
+	});
+	/*If a day of the week is clicked then hightlight that circle
+	|| if we scroll to a day of the week
+		/*hightlight that circle
+		/*unhighlight every other circle
+	If we scroll to day 1
+		/*hide the menu button
+		/*show the navigation
+*/
 
 });
+
